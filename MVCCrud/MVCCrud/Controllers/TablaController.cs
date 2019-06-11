6a647e3fc7d7bf60e33c0dfb1db940b1651cdaf4
Jose Chuman <josechuman37@gmail.com>
2019-06-11 13:35:19 -0500

    proyecto entity framework con .net

---
+++ MVCCrud/MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (masterEntities1 db = new masterEntities1())
+            {
+                lst = (from d in db.tabla
+                          select new ListTablaViewModel
+                          {
+                              Id = d.id,
+                              Nombre = d.nombre,
+                              Correo = d.correo
+                          }).ToList();
+            }
+
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    using (masterEntities1 db = new masterEntities1())
+                    {
+                        var oTabla = new tabla();
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (masterEntities1 db = new masterEntities1())
+            {
+                var oTabla = db.tabla.Find(id);
+                model.Nombre = oTabla.nombre;
+                model.Correo = oTabla.correo;
+                model.Fecha_Nacimiento = (DateTime)oTabla.fecha_nacimiento;
+                model.Id = oTabla.id;
+            }
+                return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (masterEntities1 db = new masterEntities1())
+                    {
+                        var oTabla = db.tabla.Find(model.Id);
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            using (masterEntities1 db = new masterEntities1())
+            {
+                var oTabla = db.tabla.Find(id);
+                db.tabla.Remove(oTabla);
+                db.SaveChanges();
+            }
+            return Redirect("~/Tabla/");
+        }
+    }
+}
---
+++ MVCCrud/MVCCrud/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        internal DateTime? fecha_nacimiento;
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+    }
+}
